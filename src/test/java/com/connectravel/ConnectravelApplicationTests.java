03f97b6b216550f6980a05ad15b771cc7a4a51b0
asd <p_caso@naver.com>
2023-10-25 16:20:53 +0900

    setting

---
+++ src/test/java/com/connectravel/ConnectravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
