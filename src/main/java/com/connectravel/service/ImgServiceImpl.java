3769186d46b4cbd9574fec454cc593475f7f5e49
myminimin <triplem0604@kakao.com>
2023-11-10 15:46:22 +0900

    [refactor] imgService 범용 메서드 추가하여 코드 중복 개선

--- src/main/java/com/connectravel/service/ImgServiceImpl.java
+++ src/main/java/com/connectravel/service/ImgServiceImpl.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.function.BiConsumer;
+
 @RequiredArgsConstructor
 @Service
 public class ImgServiceImpl implements ImgService {
@@ -18,70 +20,58 @@ public class ImgServiceImpl implements ImgService {
     private final TourBoardReviewImgRepository tourBoardReviewImgRepository;
     private final FileManager fileManager;
 
-
     public void addRoomImg(MultipartFile file, Long rno) {
-        String origin = file.getOriginalFilename();
-        String realName = fileManager.UUIDMaker(origin);
-        boolean is_add =fileManager.add(file,realName);
-        if(is_add)
-        {
-            Room room = Room.builder().rno(rno).build();
+        addImage(file, rno, (realName, id) -> {
+            Room room = Room.builder().rno(id).build();
             RoomImg roomImg = RoomImg.builder().imgFile(realName).room(room).build();
             roomImgRepository.save(roomImg);
-        }
+        });
     }
 
     @Override
     public void addReviewBoardImg(MultipartFile file, Long rbno) {
-        String origin = file.getOriginalFilename(); //사용자가 업로드한 파일 이름 가져오기 xxxx.png xxxx.jpg
-        String realName = fileManager.UUIDMaker(origin); //UUID 생성
-        boolean is_add =fileManager.add(file,realName); //파일 생성
-        if(is_add) //파일 생성 여부에 따라서 Db에 저장한다.
-        {
+        addImage(file, rbno, (realName, id) -> {
             ReviewBoard reviewBoard = ReviewBoard.builder().rbno(rbno).build();
             ReviewBoardImg reviewBoardImgEntity = ReviewBoardImg
                     .builder().imgFile(realName).reviewBoard(reviewBoard).build();
             reviewBoardImgRepository.save(reviewBoardImgEntity);
-        }
+        });
     }
 
     @Override
     public void addAdminBoardImg(MultipartFile file, Long abno) {
-        String origin = file.getOriginalFilename();
-        String realName = fileManager.UUIDMaker(origin);
-        boolean is_add =fileManager.add(file,realName);
-        if(is_add)
-        {
+        addImage(file, abno, (realName, id) -> {
             AdminBoard adminBoard = AdminBoard.builder().abno(abno).build();
             AdminBoardImg adminBoardImg = AdminBoardImg.builder().imgFile(realName).adminBoard(adminBoard).build();
             adminBoardImgRepository.save(adminBoardImg);
-        }
+        });
     }
 
     @Override
     public void addTourBoardImg(MultipartFile file, Long tbno) {
-        String origin = file.getOriginalFilename();
-        String realName = fileManager.UUIDMaker(origin);
-        boolean is_add = fileManager.add(file, realName);
-        if (is_add)
-        {
+        addImage(file, tbno, (realName, id) -> {
             TourBoard tourBoard = TourBoard.builder().tbno(tbno).build();
             TourBoardImg tourBoardImg = TourBoardImg.builder().imgFile(realName).tourBoard(tourBoard).build();
             tourBoardImgRepository.save(tourBoardImg);
-        }
+        });
     }
 
     @Override
     public void addTourBoardReviewImg(MultipartFile file, Long tbrno) {
-        String origin = file.getOriginalFilename();
-        String realName = fileManager.UUIDMaker(origin);
-        boolean is_add = fileManager.add(file, realName);
-        if (is_add)
-        {
+        addImage(file, tbrno, (realName, id) -> {
             TourBoardReview tourBoardReview = TourBoardReview.builder().tbrno(tbrno).build();
             TourBoardReviewImg tourBoardReviewImg = TourBoardReviewImg.builder().imgFile(realName).tourBoardReview(tourBoardReview).build();
             tourBoardReviewImgRepository.save(tourBoardReviewImg);
-        }
+        });
     }
 
+    /* 범용 메서드 */
+    private void addImage(MultipartFile file, Long entityId, BiConsumer<String, Long> saveImageConsumer) {
+        String origin = file.getOriginalFilename();
+        String realName = fileManager.UUIDMaker(origin);
+        boolean isAdd = fileManager.add(file, realName);
+        if (isAdd) {
+            saveImageConsumer.accept(realName, entityId);
+        }
+    }
 }
